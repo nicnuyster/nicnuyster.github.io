d299713603d638404031b5fbf7064a65bc2b1808
nicnuyster <shirmer.alex@gmail.com>
2026-06-28 20:15:24 +0300

    updated ver

---
+++ data.js
@@ -0,0 +1,26 @@
+const resumeData = {
+    namefirst: "Александр",
+    namesecond: "Ширмер",
+    namefamily: "Андреевич",
+    role: "Software Developer",
+    contact: {
+        email: "shirmer.alex@gmail.com",
+        github: "https://github.com/nicnuyster",
+        vk: "https://vk.com/nicnuyster",
+        telegramm: "@alexshirmer",
+        phone: "+7 (915) 705 75-91"
+    },
+    skills: {
+        languages: ["Golang", "Python", "C++", "C#"],
+        mltech: ["PyTorch", "Torchvision", "Tensorflow", "Keras", "Seaborn", "Matplotlib", "Scikit-learn"],
+        xai: ["GradCAM", "SHAP", "LIME"],
+        Theory: ["OOP", "SOLID", "ACID", "DRY", "KISS", "multythread", "RESTAPI", "CRUDL", "gRPC"],
+        things: ["SQL", "Docker", "Ollama", "GIT", "MAPLE", "Jupiter"]
+    },
+    education: {
+        bakalavr: "Математика и компьютерные науки",
+        magistr: "Математика и компьютерное моделирование",
+        eduwork: "Ширмер А. (2025). Объяснимые модели искусственного интеллекта для задач анализа мелких лабораторных
+животных в поведенческих тестах. (магистерская диссертация)",
+    }
+};
