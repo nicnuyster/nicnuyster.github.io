0d92c855fdee7cb32528517a6ec962bfda39f39c
nicnuyster <shirmer.alex@gmail.com>
2026-07-22 18:09:32 +0300

    image review

--- script.js
+++ script.js
@@ -23,7 +23,7 @@
     const carouselImages = [
         'images/image1.jpg',
         'images/image2.jpg',
-        'images/image3.jpg',
+        'images/image3.png'
     ];
 
     const autoplayInterval = 4000; // ms
